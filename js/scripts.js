be57cc2c80a9b0252e7f85957e8121c57e82f4f3
notax3l <singh15tanishq@gmail.com>
2025-02-20 21:41:36 -0500

    In the js file, created all the required datatypes and variables.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Creating an onject in key and value pairs that contain the story bits
+const storybit = {
+    1: ["heavy", "sexy", "soft", "cute", "dangerous","fluffy",],
+    2: ["cat", "dog", "robot", "wizard", "pizza","unicorn","human","alien"],
+    3: ["danced", "jumped", "sang", "whispered", "exploded","flew","disappeared"],
+};
+
+// Creating all the requireed variables
+const getWordButtons = document.querySelectorAll('.getword');
+const resetButton = document.getElementById('resetButton');
+const storyText = document.getElementById('storyText');
+
+
